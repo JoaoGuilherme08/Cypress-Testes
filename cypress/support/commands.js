f3393e2799652a224fe69b8f91b1df22466faffa
João Guilherme Cobo <joaocobo20@gmail.com>
2022-01-05 18:01:00 -0300

    Feito

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,9 +2,9 @@
 
 Cypress.Commands.add("Login", () => {
   cy.visit("https://commodity-homologation.saas-solinftec.com/login");
-  cy.get("#input-login-username").type("user.teste");
+  cy.get("#input-login-username").type(Cypress.env("user_name"));
 
-  cy.get("#input-login-password").type("user.teste4mbtlo7F3S");
+  cy.get("#input-login-password").type(Cypress.env("user_password"));
 
   cy.get("#btn-login-login").click();
 });
